1e229168d9893685f0ce4c70c8b0c899b7e42f84
elle <zqchow@163.com>
2020-02-04 07:33:22 +0800

    add sessions controller test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     login_params = params.require(:user).permit(:username, :password)
-    @user = User.find_by('lower(username) = lower(?)', login_params(:username))
+    @user = User.find_by('lower(username) = lower(?)', login_params[:username])
 
-    if @user&.authenticate(login_params(:password))
+    if @user&.authenticate(login_params[:password])
       login_as @user
       remember_me
       redirect_to root_url 
     else
-      flash.now[:warning] = @user ? 'incorrect password' : "cannot find the user: #{ login_params(:username) }"
+      flash.now[:warning] = @user ? 'incorrect password' : "cannot find the user: #{ login_params[:username] }"
       render :new
     end
   end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # login/create
+  def test_login_success
+    user = User.create!(username: 'test21', password: 'Test7pwd')
+
+    post '/login', params: { user: { username: 'test21', password: 'Test7pwd' } }
+
+    assert_response 302
+    assert_equal session[:user_id], user.id
+    assert_equal cookies[:remember_token], user.remember_token
+  end
+
+  def test_login_with_incorrect_pwd
+    user = User.create!(username: 'test21', password: 'Test7pwd')
+
+    post '/login', params: { user: { username: 'test21', password: 'Test1pwd' } }
+
+    assert_response 200
+    assert_template 'sessions/new'
+    assert_nil session[:user_id]
+    assert_nil cookies[:remember_token]
+    assert_equal 'incorrect password', flash['warning']
+  end
+
+  def test_login_with_incorrect_username
+    username = 'test31'
+    User.find_by(username: username)&.destroy!
+
+    post '/login', params: { user: { username: username, password: 'Test7pwd' } }
+
+    assert_response 200
+    assert_template 'sessions/new'
+    assert_nil session[:user_id]
+    assert_nil cookies[:remember_token]
+    assert_equal "cannot find the user: #{ username }", flash['warning']
+  end
+
+  # logout/destroy
+  def test_logout_success
+    delete '/logout'
+
+    assert_response 302
+  end
 end
