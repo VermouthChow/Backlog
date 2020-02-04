0887348be54079517f91504d616c3bd0d334b348
elle <zqchow@163.com>
2020-02-04 09:32:16 +0800

    add task model and task model test

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,21 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :content, :priority_weight, :status, presence: true
+
+  validate :content_maximum_words
+
+  enum status: [ :open, :in_progress, :closed, :deleted ]
+
+  PRIORITY = { 'urgent' => 100, 'important' => 50 }
+
+  private
+  def content_maximum_words
+    errors.add(:content, "is too long(maximum words 40)") if content&.split(/\s+/).count > 40
+  end
+
+  def calculate_priority_weight(priority_list)
+    priority_list.to_a.inject(0) { |sum, p| sum + PRIORITY[p].to_i }
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,64 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # find
+  def test_find_user_tasks
+    user = users(:one)
+    tasks = user.tasks
+
+    assert(tasks.count > 0)
+  end
+
+  # create
+  def test_task_params
+    { title: 't-test', 
+      content: 'this is a test',
+      priority: ['urgent', 'important'],
+      priority_weight: 150, 
+      status: :closed, 
+      user: users(:one) 
+    }
+  end
+
+  # valid
+  def test_create_valid_task
+    before_count = Task.count
+    task = Task.new(test_task_params)
+    task.save!
+
+    assert task.valid?
+    assert_equal before_count + 1, Task.count
+    assert_equal task.user, users(:one)
+  end
+
+  # invalid title
+  def test_create_task_with_too_long_title
+    task = Task.new(test_task_params.merge(title: 'testtesttesttesttest1'))
+
+    assert_not task.valid?
+    assert_equal ["Title is too long (maximum is 20 characters)"], task.errors.full_messages
+  end
+
+  def test_create_task_with_no_title
+    task = Task.new(test_task_params.merge(title: ''))
+
+    assert_not task.valid?
+    assert_equal ["Title can't be blank"], task.errors.full_messages
+  end
+
+  # invalid content
+  def test_create_task_with_too_long_content
+    task = Task.new(test_task_params.merge(content: 'this '*41))
+
+    assert_not task.valid?
+    assert_equal ["Content is too long(maximum words 40)"], task.errors.full_messages
+  end
+
+  def test_create_task_with_no_content
+    task = Task.new(test_task_params.merge(content: ''))
+
+    assert_not task.valid?
+    assert_equal ["Content can't be blank"], task.errors.full_messages
+  end 
 end
